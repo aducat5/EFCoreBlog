bccdf878e13ca8d059fec5623ad764c7373baf3b
Tamer Berat Çelik <aducat5@hotmail.com>
2018-11-15 10:53:45 +0300

    EntityFrameworkCore libraies added EFCore DBContext added Startup.cs modified BLL Layer and ICRUD repo pattern implemented partly
    HTML Template Implemented to Web Layer

---
+++ EFCoreBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFCoreBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
